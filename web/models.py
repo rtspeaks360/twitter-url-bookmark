9d8907fd35a939d33b1fc8a61e6d82493bee0a4d
Rishabh Thukral <rishi.feb1@gmail.com>
2017-08-24 07:44:58 +0530

    Handling heroku deployment

---
+++ web/models.py
@@ -0,0 +1,81 @@
+# -*- coding: utf-8 -*-
+# @Author: Rishabh Thukral
+# @Date:   2017-08-23 18:04:13
+# @Last Modified by:   Rishabh Thukral
+# @Last Modified time: 2017-08-24 07:21:46
+
+#importing dependencies
+import os
+import sys
+from sqlalchemy import Column, ForeignKey, Integer, String, DateTime, Table, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+from sqlalchemy.sql import func
+
+# declaring an object of the declarative base class.
+Base = declarative_base()
+
+
+#Models for application support
+class User(Base):
+	"""Provides the structure for users table in database."""
+
+	__tablename__ = "users"
+	id = Column(Integer, primary_key = True)
+	twitter_user_id = Column(String(20))
+	twitter_username = Column(String(20))
+	access_token = Column(String(40), nullable = False)
+	access_token_secret = Column(String(40), nullable = False)
+	last_updated_tweet_id = Column(String(20))
+
+	insert_time = Column(DateTime(timezone = True), nullable = False, default = func.now())
+	update_time = Column(DateTime(timezone = True), nullable = False, default = func.now())
+
+	tweets = relationship("Tweet", back_populates = "user")
+
+	@property
+	def serialize(self):
+		return {
+			'id' : self.id,
+			'twitter_username' : self.twitter_username,
+			'access_token' : self.access_token,
+			'access_token_secret' : self.access_token_secret,
+			'last_updated_tweet_id' : self.last_updated_tweet_id,
+			'insert_time' : self.insert_time,
+			'update_time' : self.update_time
+		}
+
+
+class Tweet(Base):
+	"""Provides the structure for the tweets table in the database."""
+
+	__tablename__ = "tweets"
+	id = Column(Integer, primary_key = True)
+	contact = Column(String(20), nullable = False)
+	twitter_contact_id = Column(String(20))
+	tweet_text = Column(String(140), nullable = False)
+	embedded_url = Column(String(100), nullable = False)
+	twitter_timestamp = Column(DateTime(timezone = True), nullable = False)
+
+	insert_time = Column(DateTime(timezone = True), nullable = False, default = func.now())
+	update_time = Column(DateTime(timezone = True), nullable = False, default = func.now())
+
+	user_id = Column(Integer, ForeignKey("users.id"))
+	user = relationship("Author", back_populates = "tweets")
+
+	@property
+	def serialize(self):
+		return {
+			'id' : self.id,
+			'contact' : self.contact,
+			'tweet_text' : self.tweet_text,
+			'embedded_url' : self.embedded_url,
+			'twitter_timestamp' : self.twitter_timestamp,
+			'insert_time' : self.insert_time,
+			'update_time' : self.update_time
+
+		}
+
+
+engine = create_engine("postgresql+psycopg2://rishabh:rishabh123@tapzo-url-bookmarker.cuxzbqqougwh.us-west-2.rds.amazonaws.com:5432/tapzo-url-bookmarker")
--- web/views.py
+++ web/views.py
@@ -2,10 +2,15 @@
 # @Author: Rishabh Thukral
 # @Date:   2017-08-23 02:43:53
 # @Last Modified by:   Rishabh Thukral
-# @Last Modified time: 2017-08-23 02:50:28
+# @Last Modified time: 2017-08-23 12:19:48
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
+import json
+import oauth2 as oauth
 
+
+consumer_key = "Gz5PF7GzHw6qaYZYQOLxP8Vt8"
+consumer_secret = "p2FAnTBhNDHxYsdvbYaIZWh3YD3AnpGrzCrbmylG0ZNlnvtVaa"
 
 # declaring blueprint for handling view serving activities
 main = Blueprint('main', __name__,
@@ -14,7 +19,11 @@ main = Blueprint('main', __name__,
 
 
 #declaring route for '/' endpoint
-@main.route('/')
+@main.route('/', methods = ['GET', 'POST'])
 def index():
-	# return render_template("homepage.html")
-	return "Hello world!"
+	# return 
+	if request.method == 'POST':
+		print "post request recieved"
+		return render_template("index.html")
+	else:
+		return render_template("index.html")
